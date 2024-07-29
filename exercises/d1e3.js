f293868385b5a3c5f290360c5229cb11e59b07da
wcgregory <129503132+wcgregory@users.noreply.github.com>
2024-07-29 08:23:16 +0000

    completed day1 exercise3

--- exercises/d1e3.js
+++ exercises/d1e3.js
@@ -4,6 +4,7 @@ const app = express()
 app.set('views', 'exercises/views') // <-- Don't change
 
 // TODO: Configure the app to use EJS as the view engine
+app.set('view engine', 'ejs')
 
 app.get('/d1e3', (req, res) => {
   const shopping = [
@@ -16,6 +17,7 @@ app.get('/d1e3', (req, res) => {
   ]
 
   // TODO: Render 'd1e3.ejs', passing the value of `shopping
+  res.render('d1e3', {shopping})
 })
 
 // TODO: Open d1e3.ejs and follow the instructions
