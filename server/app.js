a46e14983f3495aa5eded889950e74f9d3beefa6
wcgregory <129503132+wcgregory@users.noreply.github.com>
2024-07-29 11:14:59 +0000

    created movie list page with css styling

--- server/app.js
+++ server/app.js
@@ -1,7 +1,23 @@
 import express from 'express'
+import Movie from '../models/Movie.js'
 
 const app = express()
 
+app.set('views', 'views')
+app.set('view engine', 'ejs')  // use ejs as the view engine
+
+app.use(express.static('public'))
+app.use(express.json())
+
 // TODO: add your endpoints here
+app.get('/', async (req, res) => {
+    const movies = await Movie.findAll()
+    res.render('movie_index', { movies })
+})
+
+app.get('/movie/:movieId', async (req, res) => {
+    const movie = await Movie.findById(req.params.movieId)
+    res.render('movie_detail', { movie })
+})
 
 export default app
